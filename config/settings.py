9210e095782e694b87a349083794affa8aa3edd2
viniciusrabello <viniciusrabello@matematica.ufrj.br>
2025-04-14 19:38:14 -0300

    set load_dotenv to override=True

--- config/settings.py
+++ config/settings.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 
 # Load environment variables from .env file
-load_dotenv()
+load_dotenv(override=True)
 
 # Database settings
 DB_HOST = os.environ.get("DB_HOST")
